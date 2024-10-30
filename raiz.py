0eeb44ae4912f7d4f67eadd9e3c4ec4a154880c7
rebeca181 <rebeca.cuzco6004@utc.edu.ec>
2024-10-30 11:46:08 -0500

    raiz

---
+++ raiz.py
@@ -0,0 +1,11 @@
+def calcular_raiz_cuadrada(numero):
+    return math.sqrt(numero)
+
+numero =int (input ("ingrese el numero:"))
+print("la raiz de" , numero, "es:", calcular_raiz_cuadrada(numero))
+print(f "la raiz de {numero} es: {calcular_raiz_cuadrada(numero)]}")
+
+print("la raiz de {numero} es: calcular_raiz_cuadrada(numero)}")
+
+
+
